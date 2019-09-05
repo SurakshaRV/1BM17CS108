d12201350801302cbace3ecba555f1ac9682f069
SurakshaRV <43144535+SurakshaRV@users.noreply.github.com>
2019-09-05 19:36:03 +0530

    Update lab3b_pwd.py

--- lab3b_pwd.py
+++ lab3b_pwd.py
@@ -1,16 +1,19 @@
 import random
-
+import re
 def gen_pwd():
-	l='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@~#$%^&*()_-+={}[]|?><`,.'
-	l=list(l)
-	pwd=[]
-	while len(pwd)<14:
-		pwd.append(l[random.randint(0,89)])
-	return ''.join(pwd)
-	
+    sp='!@~#$%^&*()_-+={}[]|?><`,.'
+    l=list('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@~#$%^&*()_-+={}[]|?><`,.')
+    pwd=[]
+    while len(pwd)<14:
+        pwd.append(l[random.randint(0,78)])
+    pwd=''.join(pwd)
+    if re.search('[A-Z]', pwd) and re.search('[a-z]', pwd) and re.search('(\d)+', pwd) and any(c in sp for c in pwd) :
+        return pwd
+    else:
+        gen_pwd()
+
 choice=1
 while choice==1:
 	print("Your new password: ")
 	print(gen_pwd())
 	choice=int(input("Want new password: [yes=1|no=0]? "))
-	
